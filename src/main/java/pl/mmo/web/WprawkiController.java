cffa2441f24fb6e461c5553af92c03c3af33e6e8
Marta.Morga <marta.morga@asseco.pl>
2017-04-23 09:04:27 +0200

    wprawki param

--- src/main/java/pl/mmo/web/WprawkiController.java
+++ src/main/java/pl/mmo/web/WprawkiController.java
@@ -4,9 +4,11 @@ import java.util.Date;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class WprawkiController {
@@ -24,6 +26,12 @@ public class WprawkiController {
         model.put("msg", "Wartosc z modelu");
         model.addAttribute("data", new Date());
         return "wprawki";
+    }
+    
+    
+    @GetMapping("/wprawki2")
+    public String wprawkiParam(@RequestParam("cos") String cosParam, ModelMap model) {
+        return "wprawki z param coś" + cosParam;
     }
 
 }
